15a7c7a45524705cd8d3ea474f6d2a6b292e326d
Maf <Disan@10.192.129.134>
2014-01-24 08:10:40 +0100

    Cucumber get UserSessionId and can use it for other operations.

--- Cucumber/features/step_definitions/arsnova_login_steps.rb
+++ Cucumber/features/step_definitions/arsnova_login_steps.rb
@@ -1,5 +1,12 @@
 require 'httparty'
 
+$cookie
+
+class HTTPartyNoFollow
+  include HTTParty
+  no_follow true
+end
+
 Given(/^Arsnova is up and running$/) do
   @response = HTTParty.get("http://localhost:8080/arsnova-war/",
   :headers => { 'Content-Type' => 'text/plain'})
@@ -7,20 +14,23 @@ Given(/^Arsnova is up and running$/) do
 end
 
 When(/^I login in to arsnova as a guest$/) do
-  @response = HTTParty.get("http://localhost:8080/arsnova-war/auth/login?type=guest",
-  :headers => { 'Referer' => 'http://localhost:8080/arsnova-war/' })  
-  puts @response.headers['Set-Cookie']
-  puts @response.headers['Content-Type']
-  @response.code.should == 200
+begin
+  HTTPartyNoFollow.get("http://localhost:8080/arsnova-war/auth/login?type=guest",
+  :headers => { 'Referer' => 'http://localhost:8080/arsnova-war/' })
+  rescue HTTParty::RedirectionTooDeep => e 
+  e.response.code.should == "302"
+  $cookie = e.response.header['Set-Cookie']
+  end
 end
 
 When(/^asking for my information$/) do
-   @response = HTTParty.get("http://localhost:8080/arsnova-war/auth/",
-  :headers => { 'Content-Type' => 'text/plain', 'Cookie' => 'data'})
+  @response = HTTParty.get("http://localhost:8080/arsnova-war/auth/",
+  :headers => { 'Content-Type' => 'text/plain', 'Cookie' => $cookie})
   @response.code.should == 200
   puts @response.body
 end
 
 Then(/^I should get my information$/) do
-  pending # express the regexp above with the code you wish you had
 end
+
+
