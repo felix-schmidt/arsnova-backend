6eec17d5aa73b7b7b42c9a851eef611f574942e9
Maf <Disan@10.192.129.134>
2014-01-24 11:11:57 +0100

    steps and user story for user information and cookie handling finished. commentary included

--- Cucumber/features/arsnova_login_and_session.feature
+++ Cucumber/features/arsnova_login_and_session.feature
@@ -3,5 +3,5 @@ Feature: Hallo Name
   Scenario: Login into arsnova
     Given Arsnova is up and running
     When I login in to arsnova as a guest
-    And asking for my information
-    Then I should get my information
+    And asking for my user information
+    Then I should get my user information
--- Cucumber/features/step_definitions/arsnova_login_steps.rb
+++ Cucumber/features/step_definitions/arsnova_login_steps.rb
@@ -1,36 +1,53 @@
+#ermoeglicht das Nutzen von httparty vergleichbar wie include in java
 require 'httparty'
+require 'json'
 
-$cookie
-
+# anlegen einer neuen class und bekommt sämtliche eigenschaften von HTTParty
+# lediglich no_follow wird mit true ueberschrieben
+# verhindert das redirect nach einem request
 class HTTPartyNoFollow
   include HTTParty
   no_follow true
 end
 
 Given(/^Arsnova is up and running$/) do
+# normaler http request mit standart HTTParty gem
   @response = HTTParty.get("http://localhost:8080/arsnova-war/",
   :headers => { 'Content-Type' => 'text/plain'})
+# checkt ob der response code dem erwarteten entspricht
   @response.code.should == 200
 end
 
+
 When(/^I login in to arsnova as a guest$/) do
 begin
-  HTTPartyNoFollow.get("http://localhost:8080/arsnova-war/auth/login?type=guest",
+# HTTP request mit der modifizierten HTTParty class
+  HTTPartyNoFollow.get("http://localhost:8080/arsnova-war/auth/login?type=guest&role=SPEAKER",
   :headers => { 'Referer' => 'http://localhost:8080/arsnova-war/' })
   rescue HTTParty::RedirectionTooDeep => e 
   e.response.code.should == "302"
+# user session id wird in globale variable gespeichert
+# wird fast überall genutzt
   $cookie = e.response.header['Set-Cookie']
   end
 end
 
-When(/^asking for my information$/) do
+When(/^asking for my user information$/) do
   @response = HTTParty.get("http://localhost:8080/arsnova-war/auth/",
   :headers => { 'Content-Type' => 'text/plain', 'Cookie' => $cookie})
   @response.code.should == 200
-  puts @response.body
 end
 
-Then(/^I should get my information$/) do
+Then(/^I should get my user information$/) do
+	#parsed den json string
+	parsedString = JSON.parse(@response.body)
+	puts 
+	#prüft ob json string genau diese substrings enhaelt
+	@response.body.should include("\"username\":")
+	@response.body.should include("\"type\":")
+	@response.body.should include("\"role\":")
+	
+	#parsedString["type"].should_not == nil
 end
 
 
